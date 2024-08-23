0ee328e943cbc9c6fe34ae08cfb669388b874f2d
Metehan Sözen <metehansozen@outlook.com>
2024-08-24 02:43:41 +0300

    Add project files.

---
+++ AuthServer.Core/Entities/UserRefreshToken.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthServer.Core.Entities;
+
+public class UserRefreshToken
+{
+    public string UserId { get; set; }
+    public string Code { get; set; } //Refresh token code
+    public DateTime Expiration { get; set; }
+}
